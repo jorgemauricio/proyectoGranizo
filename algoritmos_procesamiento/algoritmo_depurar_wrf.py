44f2000bfbbd2faea689307c6442408179a36074
Jorge Mauricio <jorge.mauricio@me.com>
2017-12-07 22:36:54 -0600

    Agregar archivos

---
+++ algoritmos_procesamiento/algoritmo_depurar_wrf.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 17 16:17:25 2017
+
+@author: jorgemauricio
+"""
+
+import os
+import pandas as pd
+import numpy as np
+
+#%% fecha del pronostico
+fechaPronostico = '2013-11-05'
+
+#%% generate arrayFechas
+# Generate Days
+fechasSinDatos = []
+arrayFechas = []
+tanio, tmes, tdia = fechaPronostico.split('-')
+anio = int(tanio)
+mes = int(tmes)
+dia = int(tdia)
+
+for i in range(0,70,1):
+    if i == 0:
+        newDiaString = '{}'.format(dia)
+        if len(newDiaString) == 1:
+            newDiaString = '0' + newDiaString
+        newMesString = '{}'.format(mes)
+        if len(newMesString) == 1:
+            newMesString = '0' + newMesString
+        fecha = '{}'.format(anio)+"-"+newMesString+"-"+newDiaString
+    if i > 0:
+        dia = dia + 1
+        if mes == 2 and anio % 4 == 0:
+            diaEnElMes = 29
+        elif mes == 2 and anio % 4 != 0:
+            diaEnElMes = 28
+        elif mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12:
+            diaEnElMes = 31
+        elif mes == 4 or mes == 6 or mes == 9 or mes == 11:
+            diaEnElMes = 30
+        if dia > diaEnElMes:
+            mes = mes + 1
+            dia = 1
+        if mes > 12:
+            anio = anio + 1
+            mes = 1
+        newDiaString = '{}'.format(dia)
+        if len(newDiaString) == 1:
+            newDiaString = '0' + newDiaString
+        newMesString = '{}'.format(mes)
+        if len(newMesString) == 1:
+            newMesString = '0' + newMesString
+        fecha = '{}'.format(anio)+"-"+newMesString+"-"+newDiaString
+        # /Volumes/U/WRF_Granizo/ dirección en mi Laptop
+        tempPath = "/Volumes/U/WRF_Granizo/{}".format(fecha)
+        if os.path.exists(tempPath):
+            print("Fecha: {} *** Ok".format(fecha))
+            tempPath2 = "/Volumes/U/WRF_Granizo/{}/Vars_007".format(fecha)
+            if os.path.isfile(tempPath2):
+                print("***** Fecha {} : Ok".format(fecha))
+                for j in range(32,128,1):
+                    if j < 100:
+                        tempFileName = "Vars_0{}".format(j)
+                    else:
+                        tempFileName = "Vars_{}".format(j)
+                    tempPath3 = "/Volumes/U/WRF_Granizo/{}/{}".format(fecha, tempFileName)
+                    print("Eliminar: {}".format(tempFileName))
+                    os.remove(tempPath3)
+            else:
+                fechasSinDatos.append(fecha)
+                print("***** Fecha {} : Error".format(fecha))
+                print("Guardar para verificar")
+        else:
+            print("Fecha: {} *** Error".format(fecha))
+            arrayFechas.append(fecha)
+
+print("Días sin pronostico: {}".format("\n".join(arrayFechas)))
+print("Sin datos: {}".format("\n".join(fechasSinDatos)))
---
+++ algoritmos_procesamiento/algoritmo_generar_un_archivo.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 17 16:17:25 2017
+
+@author: jorgemauricio
+"""
+
+# librerias
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+from scipy.interpolate import griddata as gd
+from time import gmtime, strftime
+import time
+import os
+
+def main():
+    # generar lista de fechas disponibles
+    # /Volumes/U/WRF_Granizo path donde se encuentran los archivos
+    # listaDeFechas = [x for x in os.listdir('/Volumes/U/WRF_Granizo') if x.endswith('')]
+    listaDeFechas = ['2017-08-14', '2017-08-15']
+    # obtener coordenadas cañones antigranizo
+    dataAntigranizo = pd.read_csv("../data/Coordenadas_caniones.csv")
+
+    # ciclo de procesamiento de información
+    for i in listaDeFechas:
+        # nombre temporal de la ruta
+        rutaTemporalDeArchivos = "/Volumes/U/WRF_Granizo/{}".format(i)
+        print("* * * * * nombre temporal ruta",rutaTemporalDeArchivos)
+
+        # generar lista de archvos para procesamiento
+        listaDeArchivos = [x for x in os.listdir(rutaTemporalDeArchivos) if x.endswith('')]
+
+        # ciclo de procesamiento
+        for j in listaDeArchivos:
+            # nombre temporal del archivo a procesar
+            nombreTemporalDeArchivo = "{}/{}".format(rutaTemporalDeArchivos, j)
+            print("* * * * * nombre temporal archivo",nombreTemporalDeArchivo)
+            # leer datos de archivos
+            data = pd.read_csv(nombreTemporalDeArchivo)
+            print(data.head())
+            # determinar la hora de lectura
+            arrayTemporalHora = j.split('_')
+            nombreTemporalHora = int(arrayTemporalHora[1])
+            print("* * * * * nombre temporal hora",nombreTemporalHora)
+
+            # limites longitud > -106.49 y < -97.5
+            data = data.loc[data['Long'] > -106.49]
+            data = data.loc[data['Long'] < -97.5]
+
+            # limites latitud > 17.43 y < 25.23
+            data = data.loc[data['Lat'] > 17.43]
+            data = data.loc[data['Lat'] < 25.23]
+
+            # generar precipitación
+            data['Prec'] = data.apply(lambda x: convertDBZtoPrec(x['Ref (dbz)']), axis=1)
+
+            # generar granizo
+            data['Granizo'] = data.apply(lambda x: convertDBZtoGranizo(x['Ref (dbz)']), axis=1)
+
+            # obtener valores de x, y
+            lons = np.array(data['Long'])
+            lats = np.array(data['Lat'])
+
+            #%% iniciar la gráfica
+            plt.clf()
+
+            # agregar locación de Coordenadas_caniones
+            xC = np.array(dataAntigranizo['Long'])
+            yC = np.array(dataAntigranizo['Lat'])
+            #plt.scatter(xC, yC,3, marker='o', color='r', zorder=25)
+            # fig = plt.figure(figsize=(48,24))
+            m = Basemap(projection='mill',llcrnrlat=17.43,urcrnrlat=25.23,llcrnrlon=-106.49,urcrnrlon=-97.5,resolution='h')
+
+            # generar lats, lons
+            x, y = m(lons, lats)
+
+            # numero de columnas y filas
+            numCols = len(x)
+            numRows = len(y)
+
+            # generar xi, yi
+            xi = np.linspace(x.min(), x.max(), numCols)
+            yi = np.linspace(y.min(), y.max(), numRows)
+
+            # generar el meshgrid
+            xi, yi = np.meshgrid(xi, yi)
+
+            # generar zi
+            z = np.array(data['Prec'])
+            zi = gd((x,y), z, (xi,yi), method='cubic')
+
+            #generar clevs
+            step = (z.max() - z.min()) / 10
+            #clevs = np.linspace(z.min(), z.max(), 10)
+            clevs = [0,5,10,15,20,25,30,45,60,75]
+
+            #%% contour plot
+            cs = m.contourf(xi,yi,zi, clevs, zorder=5, alpha=0.5, cmap='PuBu')
+
+            # draw map details
+            #m.drawcoastlines()
+
+            #m.drawstates(linewidth=0.7)
+            #m.drawcountries()
+
+            #%% read municipios shape file
+            #m.readshapefile('shapes/MunicipiosAgs', 'Municipios')
+            m.readshapefile('../shapes/Estados', 'Estados')
+
+            m.scatter(xC, yC, latlon=True,s=1, marker='o', color='r', zorder=25)
+
+            #%% colorbar
+            cbar = m.colorbar(cs, location='right', pad="5%")
+            cbar.set_label('mm')
+            tituloTemporalParaElMapa = "Precipitación para la hora: {}".format(nombreTemporalHora)
+            plt.title(tituloTemporalParaElMapa)
+            nombreTemporalParaElMapa = "/Users/jorgemauricio/Documents/Research/Granizo/Maps/{}_{}_2.png".format(i,j)
+            plt.annotate('@2017 INIFAP', xy=(-102,22), xycoords='figure fraction', xytext=(0.45,0.45), color='g', zorder=50)
+
+            print("* * * * *", xC)
+            print("* * * * *", yC)
+            plt.savefig(nombreTemporalParaElMapa, dpi=300)
+            print('****** Genereate: {}'.format(nombreTemporalParaElMapa))
+
+
+def convertDBZtoPrec(dbz):
+    """
+    Funcion para convertir el valor de dbz a lluvia
+    param: dbz :  valor
+    """
+    rain = ((10**(dbz/10))/200)**(5/8)
+    if rain <= 1:
+        return 0
+    else:
+        return rain
+
+def convertDBZtoGranizo(dbz):
+    """
+    Funcion para convertir el valor de dbz a granizo
+    param: dbz :  valor
+    """
+    if dbz >= 55:
+        granizo = ((10**(dbz/10))/200)**(5/8)
+        if granizo <= 1:
+            return 0
+        else:
+            return granizo
+    else:
+        return 0
+
+if __name__ == '__main__':
+    main()
