82e41cfe08b15512f4f8136a0efddf90281c5eb2
Jorge Mauricio <jorge.mauricio@me.com>
2017-12-21 15:42:50 -0600

    agregar comentarios

--- algoritmos_procesamiento/algoritmo_interpolacion_horaria.py
+++ algoritmos_procesamiento/algoritmo_interpolacion_horaria.py
@@ -154,37 +154,37 @@ def main():
 	textFile.close()
 
 def convertDBZtoPrec(dbz):
-	"""
-	Función para convertir el valor de dbz a lluvia
-	param: dbz: valor
-	"""
-	rain = ((10**(dbz/10))/200)**(5/8)
-	if rain <= 1:
-		return 0.0
-	elif rain == np.inf:
-		return 0.0
-	elif rain == np.nan:
-		return 0.0
-	else:
-		return rain
+    """
+    Funcion para convertir el valor de dbz a lluvia
+    param: dbz :  valor
+    """
+    rain = ((10**(dbz/10))/200)**(5/8)
+    if rain <= 1:
+        return 0.0
+    elif rain == np.inf:
+    	return 0.0
+    elif rain == np.nan:
+        return 0.0
+    else:
+        return rain
 
 def convertDBZtoGranizo(dbz):
-	"""
-	Función para convertir el valor de dbz a lluvia
-	param: dbz: valor
-	"""
-	if dbz >= 55:
-		granizo = ((10**(dbz/10))/200)**(5/8)
-		if granizo <= 1:
-			return 0.0
-		elif granizo == np.inf:
-			return 0.0
-		elif granizo == np.nan:
-			return 0.0
-		else:
-			return granizo
-	else:
-		return 0.0
+    """
+    Funcion para convertir el valor de dbz a granizo
+    param: dbz :  valor
+    """
+    if dbz >= 55:
+        granizo = ((10**(dbz/10))/200)**(5/8)
+        if granizo <= 1:
+            return 0.0
+        elif granizo == np.inf:
+        	return 0.0
+        elif granizo == np.nan:
+        	return 0.0
+        else:
+            return granizo
+    else:
+        return 0
 
 if __name__ == '__main__':
     main()
