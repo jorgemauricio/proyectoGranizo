8424224ff1988abb0201a8deebf15031ee894f1d
jorgemauricio <jorge.ernesto.mauricio@gmail.com>
2018-03-13 11:06:11 -0600

    agregar algoritmo para generar gráficas por año

---
+++ algoritmos_procesamiento/04_examen.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+#######################################
+# Author: Jorge Mauricio
+# Email: jorge.ernesto.mauricio@gmail.com
+# Date: 2018-02-01
+# Version: 1.0
+# No. Examen 2
+# Valor: 10 puntos
+#######################################
+
+Desplegar el 7mo. día a partir de la fecha dada
+por el usuario de la siguiente manera
+
+Ingresa el año: 2016
+Ingresa el mes [1-12]: 08
+Ingresa el día [1-31]: 23
+El 7mo día a partir de la fecha es [aaaa-mm-dd] 2016-8-30
+"""
